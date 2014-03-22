53a5de51859b7c10e01b08e2509e9156102ee57b
evgenyk <evgeny@komarevtsev.net>
2014-03-23 00:56:49 +1100

    added nuget push and set correct package versions

---
+++ package.cmd
@@ -0,0 +1,9 @@
+C:\Windows\Microsoft.NET\Framework\v4.0.30319\MSBuild.exe /property:Configuration=Debug /property:Platform="Any CPU" /v:q
+
+del _GeneratedNuGetPackages /F /S /Q
+MD _GeneratedNuGetPackages
+for /f %%X IN ('dir /s /b idecom*.nuspec') do .nuget\nuget pack "%%~fX" -OutputDirectory _GeneratedNuGetPackages -Verbosity detailed
+
+CD _GeneratedNuGetPackages
+for /f %%X IN ('dir /b *.nupkg') do ..\.nuget\NuGet.exe push "%%~fX"
+CD ..
